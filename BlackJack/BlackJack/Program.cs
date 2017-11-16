0858cc304cb5a1bf2f2c819157e45a0e199eaf68
pirat <popov1vadym@gmail.com>
2017-11-16 15:10:51 +0300

    describing the structure of deck of cards

--- BlackJack/BlackJack/Program.cs
+++ BlackJack/BlackJack/Program.cs
@@ -8,8 +8,68 @@ namespace BlackJack
 {
     class Program
     {
+        public enum SuitName
+        {
+            heart,
+            diamond,
+            spade,
+            club
+        }
+        public enum CardNumber
+        {
+            six = 6, seven, eight, nine, ten, jack = 2, qeen, king, ace = 11
+        }
+        public struct CardStruct
+        {
+            public SuitName Suit;
+            public CardNumber Card;
+        }
         static void Main(string[] args)
         {
+            CardStruct[] deckOfCards = new CardStruct[]
+            {
+               new CardStruct {Card = CardNumber.six, Suit = SuitName.club},
+               new CardStruct {Card = CardNumber.seven, Suit = SuitName.club},
+               new CardStruct {Card = CardNumber.eight, Suit = SuitName.club},
+               new CardStruct { Card = CardNumber.nine, Suit = SuitName.club },
+               new CardStruct { Card = CardNumber.ten, Suit = SuitName.club },
+               new CardStruct { Card = CardNumber.jack, Suit = SuitName.club },
+               new CardStruct { Card = CardNumber.qeen, Suit = SuitName.club },
+               new CardStruct { Card = CardNumber.king, Suit = SuitName.club },
+               new CardStruct { Card = CardNumber.ace, Suit = SuitName.club },
+
+               new CardStruct {Card = CardNumber.six, Suit = SuitName.spade},
+               new CardStruct {Card = CardNumber.seven, Suit = SuitName.spade},
+               new CardStruct {Card = CardNumber.eight, Suit = SuitName.spade},
+               new CardStruct { Card = CardNumber.nine, Suit = SuitName.spade },
+               new CardStruct { Card = CardNumber.ten, Suit = SuitName.spade },
+               new CardStruct { Card = CardNumber.jack, Suit = SuitName.spade },
+               new CardStruct { Card = CardNumber.qeen, Suit = SuitName.spade },
+               new CardStruct { Card = CardNumber.king, Suit = SuitName.spade },
+               new CardStruct { Card = CardNumber.ace, Suit = SuitName.spade },
+
+               new CardStruct {Card = CardNumber.six, Suit = SuitName.diamond},
+               new CardStruct {Card = CardNumber.seven, Suit = SuitName.diamond},
+               new CardStruct {Card = CardNumber.eight, Suit = SuitName.diamond},
+               new CardStruct { Card = CardNumber.nine, Suit = SuitName.diamond },
+               new CardStruct { Card = CardNumber.ten, Suit = SuitName.diamond },
+               new CardStruct { Card = CardNumber.jack, Suit = SuitName.diamond },
+               new CardStruct { Card = CardNumber.qeen, Suit = SuitName.diamond },
+               new CardStruct { Card = CardNumber.king, Suit = SuitName.diamond },
+               new CardStruct { Card = CardNumber.ace, Suit = SuitName.diamond },
+
+               new CardStruct {Card = CardNumber.six, Suit = SuitName.heart},
+               new CardStruct {Card = CardNumber.seven, Suit = SuitName.heart},
+               new CardStruct {Card = CardNumber.eight, Suit = SuitName.heart},
+               new CardStruct { Card = CardNumber.nine, Suit = SuitName.heart },
+               new CardStruct { Card = CardNumber.ten, Suit = SuitName.heart },
+               new CardStruct { Card = CardNumber.jack, Suit = SuitName.heart },
+               new CardStruct { Card = CardNumber.qeen, Suit = SuitName.heart },
+               new CardStruct { Card = CardNumber.king, Suit = SuitName.heart },
+               new CardStruct { Card = CardNumber.ace, Suit = SuitName.heart },
+            };
+
+
         }
     }
 }
